e0c2b54b149f6046fa85f2e3c4f160b3c60d16c0
Justamandd <justamandd@gmail.com>
2020-10-27 01:43:15 -0300

    primeiro commit
    Criação das pastas no modelo MVC e criação do arquivo de conexão

---
+++ Conexao.php
@@ -0,0 +1,20 @@
+<?php 
+class Conexao{
+
+    private $servername = "localhost:3306";
+    private $username = "root";
+    private $password = "19cscrjfVITOR@";
+    private $database = "imobiliaria";
+    private $connection;
+
+    public function getConnection(){
+        if(is_null($this->connection)){
+            $this->connection = new PDO('mysql:host='.this->servername.'dbname='.$this->database, $this->username,$this->password);
+            $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->connection->exec('set names utf8');
+        }
+        return $this->connection;
+    }
+
+}
+?>
